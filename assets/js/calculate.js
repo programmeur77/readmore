e52e4663fc597ea41ed2007a2976004cef8f665c
Benoît Puech <benoit.puech@yahoo.fr>
2021-04-18 13:18:56 +0200

    Added calculation function to separate it from main code

---
+++ assets/js/calculate.js
@@ -0,0 +1,9 @@
+var checkHeight = function(paragraphHeight, messageContainerHeight) {
+		if (paragraphHeight > messageContainerHeight) {
+			return true;
+		} else {
+			return false;
+		}
+}
+
+export default checkHeight;
