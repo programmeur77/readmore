be2773dd89842e77b61036fcf2491738968ffce7
Benoît Puech <benoit.puech@yahoo.fr>
2021-04-18 13:18:04 +0200

    Removed calculating function to another js file

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+import checkHeight from './calculate.js';
+$(document).ready(function(){
+
+	const $messageContainer = $('#message-content');
+	const $paragraph = $('#show');
+	const $showMore = $('#show-more');
+	const $showLess = $('#show-less');
+
+	const $paragraphHeight = $paragraph.outerHeight();
+	const $messageContainerHeight = $messageContainer.outerHeight();
+
+	var $result = checkHeight($paragraphHeight, $messageContainerHeight);
+
+	if ($result === true) {
+		$showMore.css('display', 'block');
+		$showLess.css('display', 'none');
+		$showMore.click(function(e){
+			$messageContainer.css({
+				transition: 'height 1s',
+				height: $paragraphHeight + 'px'
+			})
+			$showMore.css('display', 'none');
+			$showLess.css('display', 'block');
+			$showLess.click(function(e) {
+				$messageContainer.css({
+					transition: 'height 1s',
+					height: $messageContainerHeight + 'px'
+				})
+				$showLess.css('display', 'none');
+				$showMore.css('display', 'block');
+			})
+		})
+	} else {
+		$showMore.css('display', 'none');
+	}
+})
